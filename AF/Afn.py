3434b8e2a78150a16a70da689b317f5a39ba4e59
manu <manudiv16@gmail.com>
2020-03-27 15:06:34 +0100

    afn to dfa complete

--- AF/Afn.py
+++ AF/Afn.py
@@ -1,6 +1,7 @@
-from typing import Dict, Any
+from typing import Dict, Any, Tuple
 
 from AF.State_fa import State_fa
+from AF.dfa import Dfa
 
 
 class Afn:
@@ -16,37 +17,48 @@ class Afn:
         if self.automaton["deterministic"]:
             raise TypeError
 
-    def determine(self):
+    def determine(self) -> dict:
         a = self.__start_state().state
         table = {tuple([a]): self.__diction[a]}
-        b = self.__next_key(table, self.__diction[a].values())
-        c = self.__morphs_key(b)
-        table[b] = c
-        i = 0
-        while i < 6:
-            b = self.__next_key(table, table[b].values())
-            c = self.__morphs_key(b)
-            table[b] = c
-            i = i + 1
+        next_key = self.__next_key(table, self.__diction[a].values())
+        all_in_table = False if next_key is None else True
+        while all_in_table:
+            table[next_key] = self.__morphs_key(next_key)
+            next_key = self.__next_key(table, table[next_key].values())
+            all_in_table = False if next_key is None else True
+        return self.procesado_de_determinacion(table)
 
-        return table
+    @staticmethod
+    def __next_key(table, args):
+        for value in args:
+            if value not in table:
+                return value
+        return None
 
-    def __morphs_key(self, key):
-        hola = {}
-        for x in self.alphabet:
-            for i in key:
-                a = self.__diction[i][x]
-                if len(a) > 0:
-                    if x in hola:
-                        hola[x] = hola[x] + a if a not in hola[x] else None
-                    else:
-                        hola[x] = a
-        return hola
+    def __morphs_key(self, key: tuple) -> dict:
+        dictionary_morphs: Dict[Any, Tuple[Any, ...]] = {}
+        for symbol in self.alphabet:
+            _set = set()
+            for elem in key:
+                morph = self.__diction[elem][symbol]
+                _set.update(morph)
+            dictionary_morphs[symbol] = tuple(_set)
+        return dictionary_morphs
 
-    def __next_key(self, table, args):
-        for x in args:
-            if x not in table:
-                return x
+    def to_dfa(self, dictionary_convert):
+        x = {'deterministic': True,
+             'alphabet': self.alphabet,
+             'states': []}
+        return Dfa(x)
+
+    def procesado_de_determinacion(self, dic):
+        print(dic)
+        l = list(dic.keys())
+        h = {l.index(key): {s: l.index(dic[key][s])
+                            for s in self.alphabet}
+             for key in l}
+
+        return
 
     def __start_state(self):
         start_state = (x for x in self.states if x.is_start())
